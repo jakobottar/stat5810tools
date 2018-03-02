bf830ba9102a9711a07d9a0e5abfd5039eb15773
Jakob Johnson <33588648+jakobottar@users.noreply.github.com>
2018-03-02 15:03:12 -0700

    Add files via upload

---
+++ R/class_sum.R
@@ -0,0 +1,16 @@
+#' Class Sum Function
+#'
+#' none
+#' @keywords logistic
+#' @export
+#' @examples
+#' class_sum()
+class_sum=function(truth,predicted){
+  xt=table(truth,round(predicted+0.000001))
+  pcc=round(100*sum(diag(xt))/sum(xt),2)
+  spec=round(100*xt[1,1]/sum(xt[1,]),2)
+  sens=round(100*xt[2,2]/sum(xt[2,]),2)
+  kap=round(kappa(xt)[1],4)
+  au=round(roc.area(truth,predicted)$A,4)
+  return(cbind(c("Percent Correctly Classified = ","Specificity = ","Sensitivity = ","Kappa =","AUC= "),c(pcc,spec,sens,kap,au)))
+}
---
+++ R/kappa.R
@@ -0,0 +1,27 @@
+#' Kappa Function
+#'
+#' none
+#' @keywords kappa 
+#' @export
+#' @examples
+#' kappa()
+kappa=function(x){
+  n=sum(x)
+  pobs=(x[1,1]+x[2,2])/n
+  pexp=(sum(x[1,])*sum(x[,1])+sum(x[2,])*sum(x[,2]))/n^2
+  kappa=(pobs-pexp)/(1-pexp)
+  t1=0
+  t2=0
+  t3=0
+  pii=x/n
+  pidot=apply(pii,1,sum)
+  pdotj=apply(pii,2,sum)
+  for(i in 1:2){
+    t1 = t1 + pii[i,i]*((1-pexp) - (1-pobs)*(pidot[i]+pdotj[i]))^2
+  }
+  t2 = pii[1,2]*(pdotj[1]+pidot[2])^2 + pii[2,1]*(pdotj[2] + pidot[1])^2
+  t3 = (pobs*pexp-2*pexp+pobs)^2
+  vhat = (t1 + t2*(1-pobs)^2 -t3)/(n*(1-pexp)^4)
+  se=sqrt(vhat)
+  return(c(kappa,se))
+}
