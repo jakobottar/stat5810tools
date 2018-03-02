b596eb3e31b010b1b7e525eb23178fbda433b023
Jakob Johnson <33588648+jakobottar@users.noreply.github.com>
2018-03-02 15:49:39 -0700

    Update class.sum.R

--- R/class.sum.R
+++ R/class.sum.R
@@ -11,6 +11,6 @@ class.sum=function(truth,predicted){
   spec=round(100*xt[1,1]/sum(xt[1,]),2)
   sens=round(100*xt[2,2]/sum(xt[2,]),2)
   kap=round(kappa(xt)[1],4)
-  au=round(roc.area(truth,predicted)$A,4)
+  au=round(verification::roc.area(truth,predicted)$A,4)
   return(cbind(c("Percent Correctly Classified = ","Specificity = ","Sensitivity = ","Kappa =","AUC= "),c(pcc,spec,sens,kap,au)))
 }
